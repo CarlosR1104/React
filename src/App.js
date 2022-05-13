65c1c638324a04bd0765c602b03dcd8775369bfc
Carlos Andres <ruizcarlosandre1@gmail.com>
2022-05-13 09:18:12 -0500

    Creando el primer proyecto react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import { ButtonUI } from "./components/UI/ButtonUI/ButtonUI";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola</h1>
+      <h2>Carlos</h2>
+      <ButtonUI/>
+    </div>
+  );
+}
+
+export default App;
